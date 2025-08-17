08907453e1b9c302a0c7aee693b1a36b9ad203ed
Jay <rright815@gmail.com>
2025-08-18 01:02:33 +0800

    49

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, input, output, Output } from '@angular/core';
+
+import { type User } from './user.model';
+
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// };
+
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+@Input({required: true}) user!: User;
+@Input({required:true}) selected!: boolean;
+@Output() select = new EventEmitter();
+
+get imagePath(){
+  return 'assets/users/' + this.user.avatar;
+}
+
+onSelectUser(){
+  this.select.emit(this.user.id);
+}
+}
