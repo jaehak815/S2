7a1cb848a8ff641708aac58d7afccf4841208274
Jay <rright815@gmail.com>
2025-08-18 23:12:42 +0800

    S2/53

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
