c62687369fb53859e2d112d0ef6422272a4e6aad
Jay <rright815@gmail.com>
2025-08-18 23:56:50 +0800

    S2/59

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
 import { NewTaskComponent } from "./new-task/new-task.component";
 import {type NewTaskData } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -16,8 +17,12 @@ export class TasksComponent {
   isAddingTask = false;
 
 
+  constructor(private tasksService:TasksService){
+
+  }
+
 get selectedUserTasks(){
-  return 
+  return this.tasksService.getUserTasks(this.userId);
 }
 
 onCompleteTask(id:string){
